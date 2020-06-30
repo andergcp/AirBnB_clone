de36c3949f7b50a1b12937365ffa4e200eef80dc
afarizap <afarizap@gmail.com>
2020-06-30 15:45:03 +0000

    task 5 reload not works, task 6

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" contains the entry point of the command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = (hbnb)
+
+    """ do_ commands methods """
+    def do_quit(self, arg):
+        raise SystemExit
+    def do_EOF(self, arg):
+        raise SystemExit
+
+    """ help_ commands methods """
+    def help_quit(self):
+        print("Quit command to exit the program")
+    def help_EOF(self):
+        print("EOF command to exit the program")
+
+    """ cmd modules """
+    def emptyline(self):
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,31 @@
-#
+#!usr/bin/env python3
+"""
+BaseModel from another one by using a dictionary
+"""
+import os
+import json
+
+
+class FileStorage:
+    """ serializes to JSON file and deserializes JSON file to instance """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ returns the dictionary __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id """
+        self.__objects = obj
+
+    def save(self):
+        """ serialzes __objects to the JSON file (path: __file_path) """
+        with open(self.__file_path, "w", encoding="utf-8") as Write_file:
+            json.dump(self.__objects, write_file)
+
+    def reload(self):
+        """ check if file.json exist & deserializes JSON file to __objects """
+        if os.path.isfile(self.__file_path) is True:
+            with open(self.__file_path, "r", encoding="utf-8") as read_file:
+                self.__objects = json.load(read_file)
