76c807b94ba9bd43bed185123e81021a87ce10f0
afarizap <afarizap@gmail.com>
2020-06-30 15:52:49 +0000

    fix pep8 6th task

--- console.py
+++ console.py
@@ -6,19 +6,21 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 
-    prompt = (hbnb)
+    prompt = "(hbnb) "
 
     """ do_ commands methods """
     def do_quit(self, arg):
         raise SystemExit
+
     def do_EOF(self, arg):
         raise SystemExit
 
     """ help_ commands methods """
     def help_quit(self):
-        print("Quit command to exit the program")
+        print("Quit command to exit the program\n")
+
     def help_EOF(self):
-        print("EOF command to exit the program")
+        print("EOF command to exit the program\n")
 
     """ cmd modules """
     def emptyline(self):
